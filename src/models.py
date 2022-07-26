779eea280e8e73d70aa302f6212ca1d52191ef95
Framsb <frambesbarrios@gmail.com>
2022-07-26 00:42:34 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -8,26 +8,52 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    username = Column(String(120), unique=True, nullable=False)
+    email = Column(String(120), unique=True, nullable=False)
+    password = Column(String(80), unique=False, nullable=False)
+    birthdate = Column(Date, nullable=False)
+    is_active = Column(Boolean(), unique=False, nullable=False)
+    profile = relationship('Profile', backref='user', uselist=False)
+    
+class Profile(Base):
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    about_me = Column(String(250), unique=True, nullable=False)
+    image = Column(String(240), nullable=True)
+    favorite_games = Column(String(50), unique=False, nullable=False)
+    region = Column(String(50), unique=False, nullable=False)
+    contact = Column(String(50), unique=True, nullable=False)
+    post_id = relationship('Post_user', backref='post', lazy=True)
+    profile_comments = relationship('Comment', backref='profile_comment', lazy=True)
 
-    def to_dict(self):
-        return {}
+class Post_user(Base):
+    id = Column(Integer, primary_key=True)
+    post_title = Column(String(150), unique=True, nullable=False)
+    post_game = Column(String(150))
+    post_description = Column(String(400), unique=True, nullable=False)
+    user_id = Column(Integer, ForeignKey('profile.id'))
+    post_comments = relationship('Comment', backref='post_comment', lazy=True)
+    likes = relationship("PostLike", backref = "post")
+
+class PostLike(Base):
+    __tablename__ = 'post_like'
+    id = Column(Integer, primary_key=True) 
+    user_id = Column(Integer, ForeignKey('profile.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+class Comment(Base):
+    id = Column(Integer, primary_key=True)
+    comment_content = Column(String(400))
+    author_id = Column(Integer, ForeignKey("profile.id"))
+    post_id = Column(Integer, ForeignKey("post_user.id"))
+
+class Friends(Base):
+    __tablename__ = 'Friends'
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey("user.id"))
+    user_to_id = Column(Integer, ForeignKey("user.id"))
 
 ## Draw from SQLAlchemy base
 try:
